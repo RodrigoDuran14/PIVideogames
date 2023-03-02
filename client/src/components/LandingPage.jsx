6578ac2bdc09e2c241ec42d29a3722e93bbb61bd
Rodrigo Martin Durán <potro_duran96@hotmail.com>
2023-03-02 01:26:29 -0300

    created landingPage and add styles

---
+++ client/src/components/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import style from "../styles/LandingPage.module.css"
+
+export default function LandingPage(){
+  return (
+    <div className={style.container}>
+      <h1>WELCOME TO VIDEOGAMES APP</h1>
+    <Link to="/home">
+      <button className={style.btn}>ACCESS</button>
+    </Link>
+    </div>
+  )
+}
+
