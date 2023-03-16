27bbf5313f35b3c56544d70f70b26ae65a9286d7
Rodrigo Martin Durán <potro_duran96@hotmail.com>
2023-03-16 07:22:15 -0300

    add styles responsive

--- client/src/views/LandingPage.jsx
+++ client/src/views/LandingPage.jsx
@@ -4,9 +4,7 @@ import style from "../styles/LandingPage.module.css"
 export default function LandingPage(){
   return (
     <div className={style.container}>
-      <div classname={style.title}>
       <h1>WELCOME TO VIDEOGAMES APP</h1>
-      </div>
     <Link to="/home">
       <button className={style.btn}>ACCESS</button>
     </Link>
